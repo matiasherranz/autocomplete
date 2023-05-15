f2f9299294b132e3e37f195ecdfe5e8d932e6b9a
Matias Herranz <matiasherranz@gmail.com>
2023-05-15 11:37:35 -0300

    Regex issue fix.

--- src/components/HighlightedText.tsx
+++ src/components/HighlightedText.tsx
@@ -5,9 +5,16 @@ interface Props {
   highlight: string
 }
 
+// Inspiration: https://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
+const escapeRegExpText = (string: string) => {
+  // $& means the whole matched string
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+}
+
 export const HighlightedText: FC<Props> = ({ text, highlight }) => {
   const parts = useMemo(
-    () => text.split(new RegExp(`(${highlight})`, 'gi')),
+    // () => text.split(new RegExp(`(${highlight})`, 'gi')),
+    () => text.split(new RegExp(`(${escapeRegExpText(highlight)})`, 'gi')),
     [text, highlight]
   )
 
